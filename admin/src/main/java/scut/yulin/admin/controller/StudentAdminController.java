36f07667f23c19784ac74d7c4cc5b34c8a883b99
ogugugugugua <1143724821@qq.com>
2021-04-10 23:35:58 +0800

    Global structure

---
+++ admin/src/main/java/scut/yulin/admin/controller/StudentAdminController.java
@@ -0,0 +1,20 @@
+package scut.yulin.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * @author xieyulin
+ * @date 10/04/2021
+ */
+@RestController
+@RequestMapping("/admin")
+@Api()
+public class StudentAdminController {
+    @RequestMapping("test")
+    public String test() {
+        return "test";
+    }
+}
