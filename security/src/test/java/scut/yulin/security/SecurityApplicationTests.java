7bdb43d779c83fd5badfaea3a4b1ddf5b13ce00c
xieyulin_i <199612052903Agu>
2021-05-19 10:46:30 +0800

    添加security模块

---
+++ security/src/test/java/scut/yulin/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package scut.yulin.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
