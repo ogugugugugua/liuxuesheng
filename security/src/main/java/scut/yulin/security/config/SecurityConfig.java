a29e11b002b1cf229873d011fd638b7e062a5b18
xieyulin_i <199612052903Agu>
2021-05-23 19:58:57 +0800

    修改SpringSecurity实现方式； 修改application.properties中白名单写法； 增加SpringUtils；

--- security/src/main/java/scut/yulin/security/config/SecurityConfig.java
+++ security/src/main/java/scut/yulin/security/config/SecurityConfig.java
@@ -10,9 +10,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import scut.yulin.security.component.DynamicAccessDecisionManager;
 import scut.yulin.security.component.DynamicSecurityFilter;
@@ -47,77 +49,78 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity
-//                .authorizeRequests();
-//        //不需要保护的资源路径允许访问
-//        for (String url : ignoreUrlsConfig().getUrls()) {
-//            registry.antMatchers(url).permitAll();
-//        }
-//        //允许跨域请求的OPTIONS请求
-//        registry.antMatchers(HttpMethod.OPTIONS)
-//                .permitAll();
-//        // 任何请求需要身份认证
-//        registry.and()
-//                .authorizeRequests()
-//                .anyRequest()
-//                .authenticated()
-//                // 关闭跨站请求防护及不使用session
-//                .and()
-//                .csrf()
-//                .disable()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                // 自定义权限拒绝处理类
-//                .and()
-//                .exceptionHandling()
-//                .accessDeniedHandler(restfulAccessDeniedHandler())
-//                .authenticationEntryPoint(restAuthenticationEntryPoint())
-//                // 自定义权限拦截器JWT过滤器
-//                .and()
-//                .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//        //有动态权限配置时添加动态权限校验过滤器
-//        if(dynamicSecurityService!=null){
-//            registry.and().addFilterBefore(dynamicSecurityFilter(), FilterSecurityInterceptor.class);
-//        }
-        httpSecurity
-            // 由于使用的是JWT，我们这里不需要csrf
-            .csrf()
-            .disable()
-            // 基于token，所以不需要session
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-            // 允许对于网站静态资源的无授权访问
-            .antMatchers(HttpMethod.GET,
-                "/",
-                "/*.html",
-                "/favicon.ico",
-                "/**/*.html",
-                "/**/*.css",
-                "/**/*.js",
-                "/swagger-resources/**",
-                "/v2/api-docs/**"
-            )
-            .permitAll()
-            // 对登录注册要允许匿名访问
-            .antMatchers("/admin/student/login", "/admin/student/register")
-            .permitAll()
-            //跨域请求会先进行一次options请求
-            .antMatchers(HttpMethod.OPTIONS)
-            .permitAll()
-            // 除上面外的所有请求全部需要鉴权认证
-            .anyRequest()
-            .authenticated();
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity
+                .authorizeRequests();
+        //不需要保护的资源路径允许访问
+        for (String url : ignoreUrlsConfig().getUrls()) {
+            registry.antMatchers(url).permitAll();
+        }
+        //允许跨域请求的OPTIONS请求
+        registry.antMatchers(HttpMethod.OPTIONS)
+                .permitAll();
+        // 任何请求需要身份认证
+        registry.and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                // 关闭跨站请求防护及不使用session
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // 自定义权限拒绝处理类
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler())
+                .authenticationEntryPoint(restAuthenticationEntryPoint())
+                // 自定义权限拦截器JWT过滤器
+                .and()
+                .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        //有动态权限配置时添加动态权限校验过滤器
+        if(dynamicSecurityService!=null){
+            registry.and().addFilterBefore(dynamicSecurityFilter(), FilterSecurityInterceptor.class);
+        }
 
-        // 禁用缓存
-        httpSecurity.headers().cacheControl();
-        // 添加JWT filter
-        httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        //添加自定义未授权和未登录结果返回
-        httpSecurity.exceptionHandling()
-            .accessDeniedHandler(restfulAccessDeniedHandler())
-            .authenticationEntryPoint(restAuthenticationEntryPoint());
+//        httpSecurity
+//            // 由于使用的是JWT，我们这里不需要csrf
+//            .csrf()
+//            .disable()
+//            // 基于token，所以不需要session
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//            .and()
+//            .authorizeRequests()
+//            // 允许对于网站静态资源的无授权访问
+//            .antMatchers(HttpMethod.GET,
+//                "/",
+//                "/*.html",
+//                "/favicon.ico",
+//                "/**/*.html",
+//                "/**/*.css",
+//                "/**/*.js",
+//                "/swagger-resources/**",
+//                "/v2/api-docs/**"
+//            )
+//            .permitAll()
+//            // 对登录注册要允许匿名访问
+//            .antMatchers("/admin/student/login", "/admin/student/register")
+//            .permitAll()
+//            //跨域请求会先进行一次options请求
+//            .antMatchers(HttpMethod.OPTIONS)
+//            .permitAll()
+//            // 除上面外的所有请求全部需要鉴权认证
+//            .anyRequest()
+//            .authenticated();
+//
+//        // 禁用缓存
+//        httpSecurity.headers().cacheControl();
+//        // 添加JWT filter
+//        httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//        //添加自定义未授权和未登录结果返回
+//        httpSecurity.exceptionHandling()
+//            .accessDeniedHandler(restfulAccessDeniedHandler())
+//            .authenticationEntryPoint(restAuthenticationEntryPoint());
     }
 
     /**
