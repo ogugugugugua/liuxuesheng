e0f2e4870a3b1168781479b6482af77cc7cec422
xieyulin_i <199612052903Agu>
2021-05-25 00:12:29 +0800

    RoleController部分实现

---
+++ admin/src/main/java/scut/yulin/admin/controller/RoleController.java
@@ -0,0 +1,37 @@
+package scut.yulin.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import scut.yulin.admin.dto.role.InsertRoleDTO;
+import scut.yulin.admin.dto.role.QueryRoleDTO;
+import scut.yulin.admin.service.RoleService;
+import scut.yulin.common.vo.ResponseVO;
+
+/**
+ * @Date 2021/5/24
+ * @Author xieyulin
+ * @Description TODO
+ **/
+@RestController
+@RequestMapping("roles")
+@Api(tags = "RoleController", value = "角色controller")
+public class RoleController {
+
+  @Autowired
+  private RoleService roleService;
+
+  @PostMapping("role")
+  public ResponseVO addNewRole(InsertRoleDTO insertRoleDTO) {
+    roleService.addNewRole(insertRoleDTO);
+    return ResponseVO.success("add new Role ok");
+  }
+
+  @GetMapping("list")
+  public ResponseVO getRoleList() {
+    return ResponseVO.success(roleService.getRoleList(new QueryRoleDTO(null)));
+  }
+}
